04f6abff4cbf5c50f24e596779d07dc8dff8553f
ThiagoPSs <psthigo@gmail.com>
2021-05-24 16:28:07 -0300

    atualização2d

---
+++ Assets/cor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cor : MonoBehaviour
+{
+    Renderer rend;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rend.material.SetColor("_Color", Random.ColorHSV());
+    }
+}
